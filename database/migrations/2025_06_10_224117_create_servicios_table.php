f002f474c6ceef4997866fc8031ee95ae415b303
Hector Jose Sanchez <14zanzezhjs@gmail.com>
2025-06-19 18:05:10 -0300

    test

--- database/migrations/2025_06_10_224117_create_servicios_table.php
+++ database/migrations/2025_06_10_224117_create_servicios_table.php
@@ -7,17 +7,18 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('servicios', function (Blueprint $table) {
-            $table->id('id_servicio');
-            $table->date('fecha_servicio');
-            $table->string('nombre_cliente', 100);
-            $table->string('direccion_cliente', 255);
-            $table->string('telefono_cliente', 20)->nullable();
-            $table->text('descripcion_trabajo');
-            $table->decimal('monto_total', 10, 2);
-            $table->string('estado_servicio', 50);
-            $table->string('electricista_asignado', 100)->nullable();
-            $table->timestamps();
-        });
+    $table->bigIncrements('id_servicio'); // en lugar de ->id()
+    $table->date('fecha_servicio');
+    $table->string('nombre_cliente', 100);
+    $table->string('direccion_cliente', 255);
+    $table->string('telefono_cliente', 20)->nullable();
+    $table->text('descripcion_trabajo');
+    $table->decimal('monto_total', 10, 2);
+    $table->string('estado_servicio', 50);
+    $table->string('electricista_asignado', 100)->nullable();
+    $table->timestamps();
+});
+
     }
 
     public function down(): void
